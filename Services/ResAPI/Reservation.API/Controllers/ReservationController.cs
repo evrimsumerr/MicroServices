18cdcf114ff269279a80968fae6b9cca11b0be26
Bekir Evrim Sümer <evrmsmr38@gmail.com>
2021-10-30 22:37:37 +0300

    Project files added.

---
+++ Services/ResAPI/Reservation.API/Controllers/ReservationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+
+        public ReservationController(IReservationService reservationService)
+        {
+            this.reservationService = reservationService;
+        }
+
+        [HttpGet("{id}")]
+        public ReservationDTO Get(int id)
+        {
+            return reservationService.GetResByBkgNumber(id);
+        }
+    }
+}
